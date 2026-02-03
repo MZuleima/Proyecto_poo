695a7c26b6aee17f2b0ea73455dec6aa9c6cd8c2
Zuleima <zuleimagordillo97@gmail.com>
2026-02-03 19:44:31 +0000

    actualizacion de clase

--- Gestor_incidencias/domain/estructura.py
+++ Gestor_incidencias/domain/estructura.py
@@ -1,4 +1,7 @@
 class sala_de_control:
     def __init__(self, nombre_salas):
-        
-        pass
+        SALAS_VALIDAS = [sala_crisis, sala_operadores, sala_datawall]
+        if nombre_salas not in SALAS_VALIDAS:
+            print(f"La sala no esta es valida")
+        self.nombre_salas = nombre_salas
+        self
